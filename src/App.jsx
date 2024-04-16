f1f735c40a841cbfa53c30559b33a75e729e53ad
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-04-16 00:41:45 -0400

    added footer links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
      <Header />
      <Outlet />
+     <Footer />
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
     <div>
+      <a href="https://github.com/gpayabyab" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://www.linkedin.com/in/gabriellepayabyab/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
     </div>
-  )
+  );
 }
