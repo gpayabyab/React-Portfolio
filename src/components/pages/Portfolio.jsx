115881de7f9cf0f1376854764a4109e14c31d48e
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-04-15 23:32:56 -0400

    added project link to portfolio.,jsx

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,28 +5,28 @@ export default function Portfolio() {
   const data = [
     {
       name: "Password Generator",
-      deployed: '',
-      repo: ''
+      deployed: 'https://gpayabyab.github.io/Password-Generator/',
+      repo: 'https://github.com/gpayabyab/Password-Generator'
   }, 
   {
       name: "Work Scheduler",
-      deployed: '',
-      repo: ''
+      deployed: 'https://gpayabyab.github.io/Work-Scheduler/',
+      repo: 'https://github.com/gpayabyab/Work-Scheduler'
   }, 
   {
       name: "Brewery Search",
-      deployed: '',
-      repo:''
+      deployed: 'https://gpayabyab.github.io/Project-1/',
+      repo:'https://github.com/gpayabyab/Project-1'
   },
   {
     name: "Plant Shelf",
-    deployed: '',
-    repo:''
+    deployed: 'https://plant-data-a769bbeb4892.herokuapp.com/',
+    repo:'https://github.com/BaharMahmud/PlantData'
   },
   {
     name: "Text Editor",
-    deployed: '',
-    repo:''
+    deployed: 'https://text-editor-rwiy.onrender.com',
+    repo:'https://github.com/gpayabyab/Text-Editor-'
   },
 ];
   return (
